56792ed1a183049fd6f91ba68d16346e5646ef0d
Daniel Toba <danieloloruntoba681@gmail.com>
2025-01-21 00:52:46 +0100

    create products

--- src/main/java/com/javastore/javastore/controller/ProductController.java
+++ src/main/java/com/javastore/javastore/controller/ProductController.java
@@ -3,10 +3,13 @@ package com.javastore.javastore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javastore.javastore.models.ProductDto;
 import com.javastore.javastore.models.Products;
 import com.javastore.javastore.services.ProductRepository;
 
@@ -16,9 +19,18 @@ public class ProductController {
     @Autowired
     private ProductRepository repo;
 
+    @GetMapping({"", "/"})
     public String showProductList(Model model) {
-        List<Products> products = repo.findAll();
+        List<Products> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
     }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("products", productDto);
+        return "products/CreateProduct";
+    }
+
 }
